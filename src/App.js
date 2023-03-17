bee9916a1b4d7ec996f1e309e5b7cf41e0763534
“jakob <jakobmalmgrem111@gmail.com>
2023-03-17 13:23:37 +0100

    did some carousel and layout, all mobile first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Slider from "./components/Slider";
+import MenWomenSection from "./components/MenWomenSection";
+import Outlet from "./components/Outlet";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Slider></Slider>
+      <MenWomenSection></MenWomenSection>
+      <Slider></Slider>
+      <MenWomenSection></MenWomenSection>
+      <Slider></Slider>
+      <Outlet></Outlet>
     </div>
   );
 }
---
+++ src/components/MenWomenSection.js
@@ -0,0 +1,22 @@
+function MenWomenSection() {
+  return (
+    <div className="menWomen-wrapper">
+      <div className="men-section">
+        <div>
+          <h2>MEN</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <h2>SHOP NOW!</h2>
+        </div>
+      </div>
+      <div className="women-section">
+        <div>
+          <h2>WOMEN</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <h2>SHOP NOW!</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MenWomenSection;
